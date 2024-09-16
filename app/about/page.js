84f00727eda5f0e42131b65748c5288c507c80db
Rishi <rishimaheshwari040@gmail.com>
2024-09-16 14:53:15 +0530

    update

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,32 @@
+import Head from 'next/head'
 import React from 'react'
 
 const About = () => {
     return (
-        <div>About </div>
+        <>
+            <Head>
+                <title>My About Title</title>
+                <meta name="description" content="This is a description of my about page" />
+
+                {/* Open Graph Tags */}
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Test Seo " />
+                <meta property="og:description" content="Test Seo is working or not " />
+                <meta property="og:url" content="https://www.yoursite.com/" />
+                <meta property="og:image" content="https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:site_name" content="Test Seo" />
+
+                {/* Twitter Cards (Optional) */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="My Page Title" />
+                <meta name="twitter:description" content="This is a description for Twitter" />
+                <meta name="twitter:image" content="https://www.yoursite.com/og-image.jpg" />
+            </Head>
+            <div>
+                About </div>
+        </>
     )
 }
 
--- app/page.js
+++ app/page.js
@@ -1,20 +1,28 @@
-import Link from 'next/link'
-import React from 'react'
+import Head from 'next/head';
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>
-      <Link href={"/"}>
-        Home
-      </Link>
-      <Link href={"/about"}>
-        about
-      </Link>
-      <Link href={"/contact"}>
-        contact
-      </Link>
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>Home - My Next.js App</title>
+        <meta name="description" content="This is the home page of my Next.js app" />
 
-export default Home
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Home - My Next.js App" />
+        <meta property="og:description" content="Description for the home page" />
+        <meta property="og:image" content="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <meta property="og:url" content="https://next-js-sable-ten.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Home - My Next.js App" />
+        <meta name="twitter:description" content="Description for the home page" />
+        <meta name="twitter:image" content="https://www.example.com/twitter-image.jpg" />
+      </Head>
+      <div>
+        <h1>Welcome to My Next.js App</h1>
+      </div>
+    </>
+  );
+}
